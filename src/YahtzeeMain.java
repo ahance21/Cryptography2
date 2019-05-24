59c90d4aed66460917d0436f89cfa4d72c8a8e03
ahance21 <42975655+ahance21@users.noreply.github.com>
2019-05-24 12:31:01 -0400

    Add files via upload

---
+++ src/YahtzeeMain.java
@@ -0,0 +1,14 @@
+
+public class YahtzeeMain
+{
+public static void main(String[] args)
+{
+Yahtzee yahtzee = new Yahtzee();
+yahtzee.rollAllDice();
+System.out.println(yahtzee.showDice());
+System.out.println("Get value of die number 3 is " + yahtzee.getADie(3));
+System.out.println(yahtzee.rollADice(3));
+System.out.println(yahtzee.showDice());
+System.out.println("After re-roll the die number 3: " + yahtzee.getADie(3));
+}
+}
